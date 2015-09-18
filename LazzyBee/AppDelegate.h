26d8169ed64bc22eda8793dfc26c0c4e5c79ff3d
nobody <hoavq@Vus-Mac-mini.local>
2015-09-18 15:33:30 +0700

    Done Google TagManager support

--- LazzyBee/AppDelegate.h
+++ LazzyBee/AppDelegate.h
@@ -8,10 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
+@class TAGManager;
+@class TAGContainer;
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (nonatomic, strong) TAGManager *tagManager;
+@property (nonatomic, strong) TAGContainer *container;
 
 @end
 
